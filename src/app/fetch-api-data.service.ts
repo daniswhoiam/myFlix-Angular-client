89e917ba01bebf36972d487f76fbf579599e1d57
daniswhoiam <daniil.belazovschi@gmail.com>
2021-07-20 13:46:37 +0200

    Added services for further api endpoints

--- src/app/fetch-api-data.service.ts
+++ src/app/fetch-api-data.service.ts
@@ -10,7 +10,24 @@ import { map } from 'rxjs/operators';
 
 // Declaring the api url that will provide data for the client app
 
-const apiUrl = 'YOUR_HOSTED_API_URL_HERE/';
+const apiUrl = 'https://daniswhoiam-myflix.herokuapp.com/';
+
+/**
+ * Making handleError available to all classes in this module
+ *
+ * @param error Error from Http Response
+ * @returns throwError function
+ */
+const handleError = (error: HttpErrorResponse): any => {
+  if (error.error instanceof ErrorEvent) {
+    console.error('Some error occured:', error.error.message);
+  } else {
+    console.error(
+      `Error Status code ${error.status}, ` + `Error body is: ${error.error}`
+    );
+  }
+  return throwError('Something bad happened; please try again later.');
+};
 
 @Injectable({
   providedIn: 'root',
@@ -21,7 +38,6 @@ export class UserRegistrationService {
   constructor(private http: HttpClient) {}
   // Making the api call for the user registration endpoint
   public userRegistration(userDetails: any): Observable<any> {
-    console.log(userDetails);
     return this.http
       .post(apiUrl + 'users', userDetails)
       .pipe(catchError(this.handleError));
@@ -32,10 +48,127 @@ export class UserRegistrationService {
       console.error('Some error occured:', error.error.message);
     } else {
       console.error(
-        `Error Status code ${error.status}, ` + 
-        `Error body is: ${error.error}`
+        `Error Status code ${error.status}, ` + `Error body is: ${error.error}`
       );
     }
     return throwError('Something bad happened; please try again later.');
+  }
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserLoginService {
+  constructor(private http: HttpClient) {}
+
+  public UserLogin(loginDetails: any): Observable<any> {
+    return this.http
+      .post(apiUrl + 'login', loginDetails)
+      .pipe(catchError(handleError));
+  }
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class GetAllMoviesService {
+  constructor(private http: HttpClient) {}
+
+  public getAllMovies(): Observable<any> {
+    return this.http.get(apiUrl + 'movies').pipe(catchError(handleError));
+  }
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class GetMovieService {
+  constructor(private http: HttpClient) {}
+
+  public getMovie(title: string): Observable<any> {
+    return this.http
+      .get(apiUrl + 'movies/' + title)
+      .pipe(catchError(handleError));
+  }
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class GetDirectorService {
+  constructor(private http: HttpClient) {}
+
+  public getDirector(name: string): Observable<any> {
+    return this.http
+      .get(apiUrl + 'directors/' + name)
+      .pipe(catchError(handleError));
+  }
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class GetGenreService {
+  constructor(private http: HttpClient) {}
+
+  public getGenre(title: string): Observable<any> {
+    return this.http
+      .get(apiUrl + 'genres/' + title)
+      .pipe(catchError(handleError));
+  }
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AddFavoriteMovieService {
+  constructor(private http: HttpClient) {}
+
+  public addFavoriteMovie(username: string, movieId: string): Observable<any> {
+    return this.http
+      .patch(apiUrl + 'users/' + username + 'movies/' + movieId, {})
+      .pipe(catchError(handleError));
+  }
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class DeleteFavoriteMovieService {
+  constructor(private http: HttpClient) {}
+
+  public deleteFavoriteMovie(
+    username: string,
+    movieId: string
+  ): Observable<any> {
+    return this.http
+      .delete(apiUrl + 'users/' + username + 'movies/' + movieId)
+      .pipe(catchError(handleError));
+  }
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UpdateUserService {
+  constructor(private http: HttpClient) {}
+
+  public updateUser(userDetails: any): Observable<any> {
+    return this.http
+      .put(apiUrl + 'users/' + userDetails.username, userDetails)
+      .pipe(catchError(handleError));
+  }
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class DeleteUserService {
+  constructor(private http: HttpClient) {}
+
+  public deleteUser(username: string): Observable<any> {
+    return this.http
+      .delete(apiUrl + 'users/' + username)
+      .pipe(catchError(handleError));
   }
 }
