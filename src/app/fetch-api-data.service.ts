540a96e27229e656855937cff4bb61e0fc4372c0
daniswhoiam <daniil.belazovschi@gmail.com>
2021-07-21 08:53:17 +0200

    Added api service template to reduce code

--- src/app/fetch-api-data.service.ts
+++ src/app/fetch-api-data.service.ts
@@ -12,38 +12,16 @@ import { map } from 'rxjs/operators';
 
 const apiUrl = 'https://daniswhoiam-myflix.herokuapp.com/';
 
-/**
- * Making handleError available to all classes in this module
- *
- * @param error Error from Http Response
- * @returns throwError function
- */
-const handleError = (error: HttpErrorResponse): any => {
-  if (error.error instanceof ErrorEvent) {
-    console.error('Some error occured:', error.error.message);
-  } else {
-    console.error(
-      `Error Status code ${error.status}, ` + `Error body is: ${error.error}`
-    );
-  }
-  return throwError('Something bad happened; please try again later.');
-};
-
-@Injectable({
-  providedIn: 'root',
-})
-export class UserRegistrationService {
-  // Inject the HttpClient module to the constructor params
-  // This will provide HttpClient to the entire class, making it available via this.http
-  constructor(private http: HttpClient) {}
-  // Making the api call for the user registration endpoint
-  public userRegistration(userDetails: any): Observable<any> {
-    return this.http
-      .post(apiUrl + 'users', userDetails)
-      .pipe(catchError(this.handleError));
-  }
-
-  private handleError(error: HttpErrorResponse): any {
+class BasicService {
+  constructor(public http: HttpClient) {}
+  
+  /**
+   * Making handleError available to all classes in this module
+   *
+   * @param error Error from Http Response
+   * @returns throwError function
+   */
+  public handleError = (error: HttpErrorResponse): any => {
     if (error.error instanceof ErrorEvent) {
       console.error('Some error occured:', error.error.message);
     } else {
@@ -52,123 +30,119 @@ export class UserRegistrationService {
       );
     }
     return throwError('Something bad happened; please try again later.');
+  };
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserRegistrationService extends BasicService {
+  // Inject the HttpClient module to the constructor params
+  // This will provide HttpClient to the entire class, making it available via this.http
+  // Making the api call for the user registration endpoint
+  public userRegistration(userDetails: any): Observable<any> {
+    return this.http
+      .post(apiUrl + 'users', userDetails)
+      .pipe(catchError(this.handleError));
   }
 }
 
 @Injectable({
   providedIn: 'root',
 })
-export class UserLoginService {
-  constructor(private http: HttpClient) {}
-
+export class UserLoginService extends BasicService {
   public UserLogin(loginDetails: any): Observable<any> {
     return this.http
       .post(apiUrl + 'login', loginDetails)
-      .pipe(catchError(handleError));
+      .pipe(catchError(this.handleError));
   }
 }
 
 @Injectable({
   providedIn: 'root',
 })
-export class GetAllMoviesService {
-  constructor(private http: HttpClient) {}
-
+export class GetAllMoviesService extends BasicService {
   public getAllMovies(): Observable<any> {
-    return this.http.get(apiUrl + 'movies').pipe(catchError(handleError));
+    return this.http.get(apiUrl + 'movies').pipe(catchError(this.handleError));
   }
 }
 
 @Injectable({
   providedIn: 'root',
 })
-export class GetMovieService {
-  constructor(private http: HttpClient) {}
-
+export class GetMovieService extends BasicService {
   public getMovie(title: string): Observable<any> {
     return this.http
       .get(apiUrl + 'movies/' + title)
-      .pipe(catchError(handleError));
+      .pipe(catchError(this.handleError));
   }
 }
 
 @Injectable({
   providedIn: 'root',
 })
-export class GetDirectorService {
-  constructor(private http: HttpClient) {}
-
+export class GetDirectorService extends BasicService {
   public getDirector(name: string): Observable<any> {
     return this.http
       .get(apiUrl + 'directors/' + name)
-      .pipe(catchError(handleError));
+      .pipe(catchError(this.handleError));
   }
 }
 
 @Injectable({
   providedIn: 'root',
 })
-export class GetGenreService {
-  constructor(private http: HttpClient) {}
-
+export class GetGenreService extends BasicService {
   public getGenre(title: string): Observable<any> {
     return this.http
       .get(apiUrl + 'genres/' + title)
-      .pipe(catchError(handleError));
+      .pipe(catchError(this.handleError));
   }
 }
 
 @Injectable({
   providedIn: 'root',
 })
-export class AddFavoriteMovieService {
-  constructor(private http: HttpClient) {}
-
+export class AddFavoriteMovieService extends BasicService {
   public addFavoriteMovie(username: string, movieId: string): Observable<any> {
     return this.http
       .patch(apiUrl + 'users/' + username + 'movies/' + movieId, {})
-      .pipe(catchError(handleError));
+      .pipe(catchError(this.handleError));
   }
 }
 
 @Injectable({
   providedIn: 'root',
 })
-export class DeleteFavoriteMovieService {
-  constructor(private http: HttpClient) {}
-
+export class DeleteFavoriteMovieService extends BasicService {
   public deleteFavoriteMovie(
     username: string,
     movieId: string
   ): Observable<any> {
     return this.http
       .delete(apiUrl + 'users/' + username + 'movies/' + movieId)
-      .pipe(catchError(handleError));
+      .pipe(catchError(this.handleError));
   }
 }
 
 @Injectable({
   providedIn: 'root',
 })
-export class UpdateUserService {
-  constructor(private http: HttpClient) {}
-
+export class UpdateUserService extends BasicService {
   public updateUser(userDetails: any): Observable<any> {
     return this.http
       .put(apiUrl + 'users/' + userDetails.username, userDetails)
-      .pipe(catchError(handleError));
+      .pipe(catchError(this.handleError));
   }
 }
 
 @Injectable({
   providedIn: 'root',
 })
-export class DeleteUserService {
-  constructor(private http: HttpClient) {}
-
+export class DeleteUserService extends BasicService {
   public deleteUser(username: string): Observable<any> {
     return this.http
       .delete(apiUrl + 'users/' + username)
-      .pipe(catchError(handleError));
+      .pipe(catchError(this.handleError));
   }
 }
